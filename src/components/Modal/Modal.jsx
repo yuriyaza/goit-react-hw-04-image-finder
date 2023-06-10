123b47d37d3efc74d0f027eb236479aa086bb7ef
yuriyaza <yuriy.aza@gmail.com>
2023-06-10 21:28:33 +0300

    Update 1

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import css from './Modal.module.css';
+import { useEffect } from "react";
+import PropTypes from "prop-types";
+import css from "./Modal.module.css";
 
-export class Modal extends React.Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onKeyDown);
-  }
+export const Modal = ({ image, modalClose }) => {
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onKeyDown);
-  }
+  useEffect(() => {
+    const onKeyDown = (e) => {
+      if (e.code === "Escape") {
+        modalClose();
+      }
+    };
+    
+    window.addEventListener('keydown', onKeyDown);
+    return () => { window.removeEventListener("keydown", onKeyDown); };
+    
+  }, [modalClose]);
 
-  onKeyDown = (e) => {
-    if (e.code === 'Escape') {
-      this.props.onModalClose();
-    }
-  };
-
-  onBackdropClick = (e) => {
+  const onBackdropClick = (e) => {
     if (e.target === e.currentTarget) {
-      this.props.onModalClose();
+      modalClose();
     }
   };
-
-  render() {
-    const { image, onModalClose } = this.props;
-    return (
-      <div className={css.overlay} onClick={this.onBackdropClick}>
-        <div className={css.modal}>
-          <button className={css.closeButton} type='button' onClick={() => { onModalClose() }}></button>
-          <img src={image.largeImageURL} alt={'Tags: ' + image.tags} />
-        </div>
+ 
+  return (
+    <div className={css.overlay} onClick={onBackdropClick}>
+      <div className={css.modal}>
+        <button
+          className={css.closeButton}
+          type="button"
+          onClick={() => {
+            modalClose();
+          }}
+        ></button>
+        <img src={image.largeImageURL} alt={"Tags: " + image.tags} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.types = {
   image: PropTypes.object.isRequired,
